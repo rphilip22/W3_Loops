0261a97f7774a57cca52520ba72cfe2ab2eff7b5
rphilip22 <reuben.philip@student.fairfield.edu>
2026-02-11 15:54:46 -0500

    updates

--- example_5.py
+++ example_5.py
@@ -3,10 +3,25 @@
 #students = ["Alice", "Bob", "Charlie", "David", "Eve"]
 #state = {"Alaska", "Arizona", "Arkansas", "California", "Colorado"}
 
-students = {"Alice": "Alaska", "Bob": "Arizona", "Charlie": "Arkansas", 
-            "David": "California", "Eve": "Colorado"}
+#students = {"Alice": "Alaska", "Bob": "Arizona", "Charlie": "Arkansas", 
+#            "David": "California", "Eve": "Colorado"}
 
-print(students["Alice"])
+#print(students["Alice"])
 
-for name in students:
-    print(name, students[name])
+#for name in students:
+#    print(name, students[name])
+
+
+#list of dictionaries example
+
+students = [
+    {"name": "Alice", "state": "Alaska", "gender": "Female"},
+    {"name": "Bob", "state": "Arizona", "gender": "Male"},
+    {"name": "Charlie", "state": "Arkansas", "gender": "Male"},
+    {"name": "David", "state": "California", "gender": "Male"},
+    {"name": "Eve", "state": "Colorado", "gender": "Female"},
+    {"name": "Colin", "state": "Connecticut", "gender": None}
+]
+
+for student in students:
+    print(student["name"], student["state"], student["gender"])
